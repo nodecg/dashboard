cf6edda5fbc17ebb3b60e34e74d2ea0a8d0166e2
Alex Van Camp <email@alexvan.camp>
2016-04-15 11:15:16 -0500

    add basic splash screen

--- app/client/webview.js
+++ app/client/webview.js
@@ -2,6 +2,7 @@
 
 const ipcRenderer = require('electron').ipcRenderer;
 
+const splash = document.getElementById('splash');
 const opening = document.getElementById('opening');
 const openingUrl = document.getElementById('opening-url');
 
@@ -21,6 +22,10 @@ document.body.appendChild(webview);
 
 ipcRenderer.send('loadMostRecentUrl');
 
+const showSplashTimeout = setTimeout(() => {
+	splash.classList.remove('hidden');
+}, 250);
+
 let loadedUrl;
 ipcRenderer.on('loadUrl', (event, url) => {
 	if (loadedUrl === url) {
@@ -29,6 +34,8 @@ ipcRenderer.on('loadUrl', (event, url) => {
 	}
 
 	console.log('loading new url into webview:', url);
+	clearTimeout(showSplashTimeout);
+	splash.classList.add('hidden');
 	webview.style.opacity = 0;
 	opening.classList.remove('hidden');
 	openingUrl.innerText = url;
