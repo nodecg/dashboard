196eebb5490748a4b0339e64011c4661d303f1e1
Alex Van Camp <email@alexvan.camp>
2016-04-16 09:52:26 -0500

    maybe fix linux deploy [ci skip]

--- .travis_prep_releases.sh
+++ .travis_prep_releases.sh
@@ -5,6 +5,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 	export RELEASE_UPDATER_FILE=$(ls dist/*-darwin-x64/*-mac.zip);
 else
 	export RELEASE_PKG_FILE=$(ls dist/nodecg-dashboard-*-amd64.deb);
+	export RELEASE_UPDATER_FILE=$(ls dist/nodecg-dashboard-*-amd64.deb);
 fi
 
 echo "Deploying $RELEASE_PKG_FILE to GitHub releases."
