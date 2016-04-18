7c59354c8a42f8e38fa0e9f5a7948995c9eb0240
Alex Van Camp <email@alexvan.camp>
2016-04-18 15:29:53 -0500

    implement UI to prompt the user to install update and restart

--- app/client/main/updater.js
+++ app/client/main/updater.js
@@ -3,12 +3,12 @@
 
 	const ipcRenderer = require('electron').ipcRenderer;
 	const update = document.getElementById('update');
-	const updateReleaseName = document.getElementById('update-releaseName');
+	const updateLabel = document.getElementById('update-label');
 	const updateNow = document.getElementById('update-now');
 	const updateLater = document.getElementById('update-later');
 
 	ipcRenderer.on('updateDownloaded', (event, releaseName) => {
-		updateReleaseName.innerText = releaseName;
+		updateLabel.innerText = `A new version (${releaseName}) is ready to install.`;
 		update.style.display = 'flex';
 	});
 
