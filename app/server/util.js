758eccd71bd2af9ae22b5100694d85d60cc6fcd5
Alex Van Camp <email@alexvan.camp>
2016-04-19 08:50:27 -0500

    remove busted nslog dep for now

--- app/server/util.js
+++ app/server/util.js
@@ -2,18 +2,21 @@
 
 const isDev = require('electron-is-dev');
 
-let _log;
+let _log = function () {};
 
 if (isDev) {
 	_log = function (...args) {
 		console.log(args);
 	};
-} else {
+}
+
+// Commented out until https://github.com/atom/node-nslog/issues/12 is resolved
+/* else {
 	const nsLog = require('nslog');
 	_log = function (...args) {
 		nsLog(args);
 	};
-}
+}*/
 
 module.exports = {
 	isDev,
